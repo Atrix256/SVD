add21eb7129b3a8f0a796c00851f7185aaa44e21
Alan Wolfe <alan.wolfe@gmail.com>
2022-07-12 20:16:59 -0700

    image decomp progress

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "Eigen/Eigenvalues"
 #include "Eigen/SVD"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+#include <vector>
 #include <iostream>
 
 using namespace Eigen;
@@ -74,7 +83,7 @@ int main(int argc, char** argv)
 		auto V = svd.matrixV();
 		auto sigma = svd.singularValues().asDiagonal().toDenseMatrix();
 
-		cout << "C = \n" << C << "\n\n";
+		cout << "C = \n" << newC << "\n\n";
 
 		cout << "V = \n" << V << "\n\n";
 
@@ -83,6 +92,65 @@ int main(int argc, char** argv)
 		cout << "C * V = \n" << newC * V << "\n\n";
 
 		cout << "Principle Direction = \n" << (newC * V).col(0).normalized() << "\n\n";
+	}
+
+	// Do truncated SVD on an image
+	{
+		// load the image
+		int width, height, components;
+		// TODO: try with larger image!
+		//unsigned char* pixels = stbi_load("scenery.png", &width, &height, &components, 3);
+		unsigned char* pixels = stbi_load("lozman.png", &width, &height, &components, 3);
+		components = 3;
+
+		// convert it to double, and also give each component it's own row
+		std::vector<double> mtxpixels(width * height * components);
+		const unsigned char* srcPixel = pixels;
+		for (size_t iy = 0; iy < height; ++iy)
+		{
+			for (size_t ix = 0; ix < width; ++ix)
+			{
+				for (size_t ic = 0; ic < components; ++ic)
+				{
+					mtxpixels[(iy * components + ic) * width + ix] = float(*srcPixel) / 255.0;
+					srcPixel++;
+				}
+			}
+		}
+		stbi_image_free(pixels);
+
+		// make the matrix
+		Map<MatrixXd> mf(mtxpixels.data(), height * components, width);
+
+		// Singular value decomposition
+		BDCSVD<MatrixXd> svd(mf, ComputeThinU | ComputeThinV);
+		auto U = svd.matrixU();
+		auto V = svd.matrixV();
+		auto sigma = svd.singularValues().asDiagonal().toDenseMatrix();
+
+		int percents[] =
+		{
+			100,
+			75,
+			50,
+			25,
+			0
+		};
+
+		for (int imageIndex = 0; imageIndex < _countof(percents); ++imageIndex)
+		{
+			cout << "mf is " << mf.rows() << " x " << mf.cols() << "\n";
+			cout << "U is " << U.rows() << " x " << U.cols() << "\n";
+			cout << "sigma is " << sigma.rows() << " x " << sigma.cols() << "\n";
+			cout << "V is " << V.rows() << " x " << V.cols() << "\n";
+
+			// TODO: how to truncate?
+
+			auto result = U * sigma * V.transpose();
+			cout << "result is " << result.rows() << " x " << result.cols() << "\n";
+			//cout << "sigma = \n" << sigma << "\n\n";
+			int ijkl = 0;
+		}
 	}
 
 	return 0;
